b4989cfb45db9e051336e386a3e8bed8376ada23
Mark Visser <mjmvisser@gmail.com>
2012-11-30 17:34:01 -0500

    removed override of PYTHONPATH

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 export QTDIR=/prod/tools/common/qt5
 export PATH=$QTDIR/bin:$PATH
 export QML_IMPORT_PATH=~/workspace/pug/imports:~/workspace/pug/qml
-export PYTHONPATH=~/workspace/pug/python
+#export PYTHONPATH=~/workspace/pug/python
 #export MAKEFLAGS=-j8
 export PUGROOT=~/workspace/pug/qml/Mokko.qml
 export PATH=~/workspace/pug/bin:$PATH
